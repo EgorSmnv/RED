73fa7f8948f43f9588530ab7ea99edddcb32057d
ProD1J <dimapro2001@gmail.com>
2021-12-23 23:09:54 +0500

    edited interface

--- RED/src/Red/calc_interface.java
+++ RED/src/Red/calc_interface.java
@@ -6,6 +6,7 @@ import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
+import javax.swing.plaf.synth.ColorType;
 
 public class calc_interface {
 	private JTextField Price = new JTextField(); 
@@ -27,18 +28,18 @@ public class calc_interface {
 		JFrame a = new JFrame("Calculator"); 
 		
 		JButton b = new JButton("Рассчитать"); 
-		b.setBounds(20,550,200,40); 
+		b.setBounds(55,550,200,40); 
 		b.addActionListener(new ButtonAction()); 
 		a.add(b); 
 		 
 		 
-		Price.setBounds(20,90,200,30); 
+		Price.setBounds(30,90,250,30); 
 		a.add(Price); 
 		 
-		FirstPay.setBounds(20,150,200,30); 
+		FirstPay.setBounds(30,150,250,30); 
 		a.add(FirstPay); 
 		 
-		Time.setBounds(20,210,200,30); 
+		Time.setBounds(30,210,250,30); 
 		a.add(Time); 
 		 
 		JLabel L1 = new JLabel("Стоимость недвижимости:"); 
@@ -54,7 +55,7 @@ public class calc_interface {
 		a.add(L3); 
 		 
 		Goal = new JComboBox<String>(courses); 
-		Goal.setBounds(20,30,200,30); 
+		Goal.setBounds(30,30,250,30); 
 		a.add(Goal); 
 		 
 		JLabel L4 = new JLabel("Цель кредита:"); 
@@ -81,25 +82,54 @@ public class calc_interface {
 		 
 		JLabel L8 = new JLabel("Переплата:"); 
 		L8.setBounds(20,470,200,30); 
-		a.add(L8); 
+		a.add(L8);
+		
+		JLabel L9 = new JLabel("<html> <font color=#777777>Разработка: <font/> <html/>"); 
+		L9.setBounds(20,605,400,30);
+		a.add(L9); 
+		
+		JLabel L10 = new JLabel("<html> <font color=#777777>Семенов Е.Б. <font/> <html/>"); 
+		L10.setBounds(25,630,400,30); 
+		a.add(L10); 
+		
+		JLabel L11 = new JLabel("<html> <font color=#777777>Зачетная книжка: 19130102<font/> <html/>"); 
+		L11.setBounds(25,645,400,30); 
+		a.add(L11); 
+		
+		JLabel L12 = new JLabel("<html> <font color=#777777>Аюпов Р.М.<font/> <html/>"); 
+		L12.setBounds(25,670,400,30); 
+		a.add(L12); 
+		
+		JLabel L13 = new JLabel("<html> <font color=#777777>Зачетная книжка: 19130546<font/> <html/>"); 
+		L13.setBounds(25,685,400,30); 
+		a.add(L13); 
+		
+		JLabel L14 = new JLabel("<html> <font color=#777777>Прохоров Д.А<font/> <html/>"); 
+		L14.setBounds(25,710,400,30); 
+		a.add(L14); 
+		
+		JLabel L15 = new JLabel("<html> <font color=#777777>Зачетная книжка: 19130108<font/> <html/>"); 
+		L15.setBounds(25,725,400,30); 
+		a.add(L15); 
 		 
-		CreditSize.setBounds(20,380,200,30);
+		CreditSize.setBounds(30,380,250,30);
 		CreditSize.setEditable(false);
 		a.add(CreditSize); 
 		 
-		MonthPay.setBounds(20,440,200,30);
+		MonthPay.setBounds(30,440,250,30);
 		MonthPay.setEditable(false);
 		a.add(MonthPay); 
 		 
-		OverPay.setBounds(20,500,200,30);
+		OverPay.setBounds(30,500,250,30);
 		OverPay.setEditable(false);
 		a.add(OverPay); 
+		
+		
 		 
-		 
-		 
-		a.setSize(400,650); 
+		a.setSize(310,800); 
 		a.setLayout(null); 
-		a.setVisible(true); 
+		a.setVisible(true);
+		a.setResizable(false);
 	} 
 	 
 	public JTextField[] getArray() { 
